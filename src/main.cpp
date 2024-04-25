04fffba8733b8e7c76e136048ed85aeb2066141d
Mircea <mihail.mircea12@gmail.com>
2024-04-25 20:00:59 +0300

    major restructuring to accomodate a working menu. In broad terms i made some objects to make the code more organised and made the queue only accept one argument, which is the instance of the menu that is going to be displayed (as a menu object).

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,16 @@
 #include <GxEPD2_BW.h>
 #include <Fonts/FreeMonoBold9pt7b.h>
 // my include files
-#include <displayTask.h>
+#include "displayTask.h"
+#include "menu.h"
+#include "bikeCalc.h"
+#include "HardwareUtility.h"
 
-// pins
-#define HALL_SENSOR_PIN GPIO_NUM_39
+// waveshare E-Paper display config
+#define SERIAL_BITRATE 115200
+#define WAVESHARE_REFRESH_TIME_MS 2
 
+// tasks and task comms related
 // lowest priority, no reason to have special priority all the time
 #define DEFAULT_TASK_PRIORITY tskIDLE_PRIORITY
 #define DEFAULT_TASK_STACK_SIZE 2048
@@ -21,12 +26,7 @@
 #define FULL_EPAPER_REFRESH_PERIOD_MS (180 * MS_TO_SECONDS)
 // after full refresh keep printing for a bit to warm up the display
 #define CONTINUOUS_PRINT_PERIOD_MS 15000 
-#define SEND_MEASUREMENTS_PERIOD 2000
-
-//////////////////////// Speed coomputing related
-// #define PI 3.1415
-#define WHEEL_DIAMETER_MM 700UL
-#define WHEEL_PERIMETER_MM (WHEEL_DIAMETER_MM * PI)
+#define SEND_MEASUREMENTS_PERIOD 1500
 
 //////////////////////// Immage related
 #define NUMBER_OX_OFFSET 20
@@ -42,6 +42,7 @@ QueueHandle_t g_communicationQueue;
 void initPins()
 {
     // ePaper display pins
+    pinMode(CHANGE_SUBMENU_PIN, INPUT_PULLDOWN);
     pinMode(CHIP_SELECT, OUTPUT);
     pinMode(DATA_COMMAND, OUTPUT);
     pinMode(RST, OUTPUT);
@@ -54,16 +55,6 @@ void initPins()
     // digitalWrite(HALL_SENSOR_PIN, HIGH); // activate internal pullup resistor
 }
 
-void doNormalRefreshSequence(unsigned long p_lastFullRefresh, int p_speedToPrint, int p_previousReceivedSpeed)
-{
-    if(millis() - p_lastFullRefresh < CONTINUOUS_PRINT_PERIOD_MS || p_previousReceivedSpeed != p_speedToPrint)
-    {
-        clearImmage(g_matrixToDisplay);
-        addNumberCentered(g_matrixToDisplay, p_speedToPrint);    
-        displayImmage(g_matrixToDisplay, true);
-    }
-}
-
 // desired workflow:
 //      * wait for message from sensor reading task,
 //      * display received number 
@@ -72,117 +63,88 @@ void doNormalRefreshSequence(unsigned long p_lastFullRefresh, int p_speedToPrint
 // important details:
 //      * only update once per new speed value
 //      * if received speed is the same as the current speed don't update
-//      * after full refresh update continuously for some seconds or so "to warm" up pixels 
+//      * after full refresh update continuously for some seconds or so to "warm up" pixels 
 //      check bounds when printing anything to the screen
 //      write rescale funtion to print the number as big or as small as you want
 void displayManagement(void *args)
 {    
     resetPanel();
-    g_display.init(115200, true, 2, false);  //for Waveshare boards with 2ms reset pulse
+    g_display.init(SERIAL_BITRATE, true, WAVESHARE_REFRESH_TIME_MS, false);
+    
     clearImmage(g_matrixToDisplay);
     g_display.setRotation(ORIENTATION_HORIZONTAL);
     g_display.clearScreen();
 
-    int previousReceivedSpeed = 0;
     unsigned long lastFullRefresh = 0;
+    Menu menu;
+
+    // one full white refresh
+    clearImmage(g_matrixToDisplay);
+    displayImmage(g_matrixToDisplay, false);
 
     while(true)
     {
-        int speedToPrint = previousReceivedSpeed;
         // wait for as long as possible to receive the speed to print
-        xQueueReceive(g_communicationQueue, &speedToPrint, SEND_DATA_DELAY_TICKS);
-        // Serial.println("Received message");
+        xQueueReceive(g_communicationQueue, &menu, SEND_DATA_DELAY_TICKS);
+
+        // enter menu and display the appropriate thing on the matrix then display it regardless of what it is
+        menu.getImmage(g_matrixToDisplay);
 
         // needs a full refresh
         if(millis() - lastFullRefresh > FULL_EPAPER_REFRESH_PERIOD_MS)
         {
-            lastFullRefresh = millis();
-            clearImmage(g_matrixToDisplay);
-            addNumberCentered(g_matrixToDisplay, speedToPrint);    
-            displayImmage(g_matrixToDisplay, false);
-         
-            clearImmage(g_matrixToDisplay);
+            lastFullRefresh = millis();   
             displayImmage(g_matrixToDisplay, false);
         }
-        // average refresh
+        // fast refresh,
         else
         {        
-            doNormalRefreshSequence(lastFullRefresh, speedToPrint, previousReceivedSpeed);
+            displayImmage(g_matrixToDisplay, true);
         }
-
-        previousReceivedSpeed = speedToPrint;
     }
-}
-
-#define MM_TO_KM 1000000UL
-#define MILLIS_TO_MICROS 1000UL
-#define MICROS_TO_SECONDS 1000000UL
-#define SECONDS_TO_HOURS 3600UL
-#define DEBOUNCE_PERIOD_MS 50UL
-
-
-int getSpeedKmPerH(int64_t p_lastWheelDetectionTime)
-{
-    int64_t fullSpinDurationMicros = esp_timer_get_time() - p_lastWheelDetectionTime;
-    int64_t computedSpeed = (WHEEL_PERIMETER_MM * SECONDS_TO_HOURS / fullSpinDurationMicros);
-    return computedSpeed > 100 ? 99 : computedSpeed;
-}
-
-bool debounceActiveLowSenor(int64_t p_lastSensorDetectionTime, const int p_sensorPin)
-{
-    bool detectedActivity = !digitalRead(p_sensorPin);
-    if(esp_timer_get_time() - p_lastSensorDetectionTime < (DEBOUNCE_PERIOD_MS * MILLIS_TO_MICROS))
-    {
-        return HIGH; // active low, return HIGH when innactive
-    }
-    return detectedActivity;
 }
 
 void measurementTask(void *args)
 {
     unsigned long lastMeasure = 0;
-    int speed = 0;
-    bool hallHasSwitched = false;
+    uint8_t speed = 0;
+    Menu menu;
+    BikeCalc bikeCalc;
 
     int64_t lastWheelDetectionTime = 0;
     bool sendingLatestSpeed = true;
 
+    HardwareUtility hwUtil;
+
     while(true)
     {
-        if(millis() - lastMeasure > SEND_MEASUREMENTS_PERIOD)
+        // send latest speed every x seconds only if the queue is empty (the display has seen the previous speed sent)
+        unsigned portBASE_TYPE queueLength = uxQueueMessagesWaiting(g_communicationQueue);
+        if(millis() - lastMeasure > SEND_MEASUREMENTS_PERIOD && queueLength == 0)
         {
             lastMeasure = millis();
-            unsigned portBASE_TYPE queueLength = uxQueueMessagesWaiting(g_communicationQueue);
 
-            if(queueLength == 0)
+            if(sendingLatestSpeed)
             {
-                if(sendingLatestSpeed)
-                {
-                    xQueueSend(g_communicationQueue, &speed, SEND_DATA_DELAY_TICKS);
-                }
-                else
-                {
-                    int approximatedSpeed = getSpeedKmPerH(lastWheelDetectionTime);
-                    xQueueSend(g_communicationQueue, &approximatedSpeed, SEND_DATA_DELAY_TICKS);
-                }
+                menu.update(speed);
+                xQueueSend(g_communicationQueue, &menu, SEND_DATA_DELAY_TICKS);
+            }
+            else
+            {
+                int approximatedSpeed = bikeCalc.approximateVelocity(lastWheelDetectionTime);
+                menu.update(approximatedSpeed);
+                xQueueSend(g_communicationQueue, &menu, SEND_DATA_DELAY_TICKS);
             }
             
             sendingLatestSpeed = false;
         }
 
-        // the sensor is active low
-        bool detectedMagnet = debounceActiveLowSenor(lastWheelDetectionTime, HALL_SENSOR_PIN);
-        if(detectedMagnet && hallHasSwitched)
+        if(hwUtil.detectedSensor())
         {
-            speed = getSpeedKmPerH(lastWheelDetectionTime);
+            speed = bikeCalc.recordVelocity(lastWheelDetectionTime);
             lastWheelDetectionTime = esp_timer_get_time();
 
             sendingLatestSpeed = true;
-            hallHasSwitched = false;
-        }
-        if(!detectedMagnet)
-        {
-            hallHasSwitched = true;
         }
         // taskYIELD();
     }
@@ -193,7 +155,7 @@ void setup()
     initPins();
     Serial.begin(115200);
 
-    g_communicationQueue = xQueueCreate(QUEUE_SIZE, sizeof(int));
+    g_communicationQueue = xQueueCreate(QUEUE_SIZE, sizeof(Menu));
     if(g_communicationQueue == NULL){
         Serial.println("Error creating the queue\n");
     }
