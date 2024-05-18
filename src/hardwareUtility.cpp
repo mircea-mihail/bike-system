0963f0d82162f1cc12134ff4f96258eaae21322c
Mircea <mihail.mircea12@gmail.com>
2024-05-18 20:09:00 +0300

    made the sensor detector detect every state change from down to up, leaving the filtering to the calculator

--- src/hardwareUtility.cpp
+++ src/hardwareUtility.cpp
@@ -10,23 +10,25 @@
 //     return detectedActivity;
 // }
 
+// no filtering on sensor reading -> all filtering is done in the speed recording area
 bool HardwareUtility::detectedSensor()
 {
-    unsigned long time = millis();
+    // unsigned long time = millis();
     int state = digitalRead(HALL_SENSOR_PIN);
 
     // if state just changed from low to high do something else wait for state change
     if(m_sensor.m_prevState != state)
     {
         // if detected magnet and it's been a while since last reading then probabily valid state read
-        if(state == LOW && m_sensor.m_prevTime - time > SENSOR_DEBOUNCE_PERIOD_MS)
+        // if(state == LOW && m_sensor.m_prevTime - time > SENSOR_DEBOUNCE_PERIOD_MS)
+        if(state == LOW)
         {
-            m_sensor.m_prevTime = time;
+            // m_sensor.m_prevTime = time;
             m_sensor.m_prevState = state;
             return true;
         }
     
-        m_sensor.m_prevTime = time;
+        // m_sensor.m_prevTime = time;
     }
     
     m_sensor.m_prevState = state;
