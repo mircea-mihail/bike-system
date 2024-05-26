fe77060eec4a768139e4270bb2475427bb957e7c
Mircea <mihail.mircea12@gmail.com>
2024-05-26 20:52:34 +0300

    fixed the filtering function

--- src/bikeCalc.cpp
+++ src/bikeCalc.cpp
@@ -7,17 +7,18 @@ TripData BikeCalc::recordDetection()
 {        
     int64_t fullSpinDurationMicros = esp_timer_get_time() - m_lastWheelDetectionTime;
     double previousVelocity = m_data.m_currentVelocity;
-    m_data.m_currentVelocity = (WHEEL_PERIMETER_MM * SECONDS_TO_HOURS / (double)fullSpinDurationMicros);
-    TripData dataToSend = m_data;
+    double currentVelocity = (WHEEL_PERIMETER_MM * SECONDS_TO_HOURS / (double)fullSpinDurationMicros);
 
     // ignore reading, most likely fake
-    if(m_data.m_currentVelocity - m_data.m_previousVelocity > MAX_DELTA_VELOCITY_KMPH 
-        || m_data.m_currentVelocity > MAX_POSSIBLE_VELOCITY 
-        || m_data.m_currentVelocity < MIN_POSSIBLE_VELOCITY)
+    if(currentVelocity - previousVelocity > MAX_DELTA_VELOCITY_KMPH 
+        || currentVelocity > MAX_POSSIBLE_VELOCITY 
+        || currentVelocity < MIN_POSSIBLE_VELOCITY)
     {
         // ignore, send previous data recording
-        return dataToSend;
+        return m_data;
     }
+    
+    m_data.m_currentVelocity = currentVelocity;
     m_data.m_magnetDetections ++;
 
     int64_t distanceInKm = m_data.m_magnetDetections * WHEEL_PERIMETER_MM * SECONDS_TO_HOURS; // / MM_TO_KM * MICROS_TO_SECONDS (they cancel out)
@@ -26,7 +27,7 @@ TripData BikeCalc::recordDetection()
     m_lastWheelDetectionTime = esp_timer_get_time();
     m_data.m_previousVelocity = previousVelocity;
 
-    return dataToSend;
+    return m_data;
 }
 
 
