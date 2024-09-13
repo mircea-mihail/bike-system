92e0816f9b345ff3ec6883fb12f1e48c0ec71e43
Mircea <mihail.mircea12@gmail.com>
2024-09-14 00:23:56 +0300

    refactored the code and lowered the camera's resolution to match the one on the final project

--- bike-camera/src/main.cpp
+++ bike-camera/src/main.cpp
@@ -46,73 +46,68 @@
 
 int pictureNumber = 0;
 
-void setup() 
+void configureCamera(camera_config_t &p_camConf)
 {
-	WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); //disable brownout detector
-	
-	Serial.begin(115200);
-	//Serial.setDebugOutput(true);
-	//Serial.println();
-	
-	camera_config_t config;
-	config.ledc_channel = LEDC_CHANNEL_0;
-	config.ledc_timer = LEDC_TIMER_0;
-	config.pin_d0 = Y2_GPIO_NUM;
-	config.pin_d1 = Y3_GPIO_NUM;
-	config.pin_d2 = Y4_GPIO_NUM;
-	config.pin_d3 = Y5_GPIO_NUM;
-	config.pin_d4 = Y6_GPIO_NUM;
-	config.pin_d5 = Y7_GPIO_NUM;
-	config.pin_d6 = Y8_GPIO_NUM;
-	config.pin_d7 = Y9_GPIO_NUM;
-	config.pin_xclk = XCLK_GPIO_NUM;
-	config.pin_pclk = PCLK_GPIO_NUM;
-	config.pin_vsync = VSYNC_GPIO_NUM;
-	config.pin_href = HREF_GPIO_NUM;
-	config.pin_sccb_sda = SIOD_GPIO_NUM;
-	config.pin_sccb_scl = SIOC_GPIO_NUM;
-	config.pin_pwdn = PWDN_GPIO_NUM;
-	config.pin_reset = RESET_GPIO_NUM;
-	config.xclk_freq_hz = 20000000;
-	config.pixel_format = PIXFORMAT_JPEG; 
-	
-	if(psramFound())
-	{
-		config.frame_size = FRAMESIZE_UXGA; // FRAMESIZE_ + QVGA|CIF|VGA|SVGA|XGA|SXGA|UXGA
-		config.jpeg_quality = 10;
-		config.fb_count = 2;
-	} 
-	else 
-	{
-		config.frame_size = FRAMESIZE_SVGA;
-		config.jpeg_quality = 12;
-		config.fb_count = 1;
-	}
-	
-	// Init Camera
-	esp_err_t err = esp_camera_init(&config);
-	if (err != ESP_OK) 
-	{
-		Serial.printf("Camera init failed with error 0x%x", err);
-		return;
-	}
-	
-	//Serial.println("Starting SD Card");
+	p_camConf.ledc_channel = LEDC_CHANNEL_0;
+	p_camConf.ledc_timer = LEDC_TIMER_0;
+	p_camConf.pin_d0 = Y2_GPIO_NUM;
+	p_camConf.pin_d1 = Y3_GPIO_NUM;
+	p_camConf.pin_d2 = Y4_GPIO_NUM;
+	p_camConf.pin_d3 = Y5_GPIO_NUM;
+	p_camConf.pin_d4 = Y6_GPIO_NUM;
+	p_camConf.pin_d5 = Y7_GPIO_NUM;
+	p_camConf.pin_d6 = Y8_GPIO_NUM;
+	p_camConf.pin_d7 = Y9_GPIO_NUM;
+	p_camConf.pin_xclk = XCLK_GPIO_NUM;
+	p_camConf.pin_pclk = PCLK_GPIO_NUM;
+	p_camConf.pin_vsync = VSYNC_GPIO_NUM;
+	p_camConf.pin_href = HREF_GPIO_NUM;
+	p_camConf.pin_sccb_sda = SIOD_GPIO_NUM;
+	p_camConf.pin_sccb_scl = SIOC_GPIO_NUM;
+	p_camConf.pin_pwdn = PWDN_GPIO_NUM;
+	p_camConf.pin_reset = RESET_GPIO_NUM;
+	p_camConf.xclk_freq_hz = 20000000;
+	p_camConf.pixel_format = PIXFORMAT_JPEG; 
+
+	p_camConf.frame_size = FRAMESIZE_CIF;
+	p_camConf.jpeg_quality = 12;
+	p_camConf.fb_count = 1;
+
+	// if(psramFound())
+	// {
+	// 	p_camConf.frame_size = FRAMESIZE_UXGA; // FRAMESIZE_ + QVGA|CIF|VGA|SVGA|XGA|SXGA|UXGA
+	// 	p_camConf.jpeg_quality = 10;
+	// 	p_camConf.fb_count = 2;
+	// } 
+	// else 
+	// {
+	// 	p_camConf.frame_size = FRAMESIZE_SVGA;
+	// 	p_camConf.jpeg_quality = 12;
+	// 	p_camConf.fb_count = 1;
+	// }
+}
+
+bool checkSD()
+{
 	if(!SD_MMC.begin())
 	{
 		Serial.println("SD Card Mount Failed");
-		return;
+		return false;
 	}
 	
 	uint8_t cardType = SD_MMC.cardType();
 	if(cardType == CARD_NONE)
 	{
 		Serial.println("No SD Card attached");
-		return;
+		return false;
 	}
-		
-	camera_fb_t * fb = NULL;
-	
+	return true;
+}
+
+void takePicture(int p_pictureNumber)
+{
+	camera_fb_t *fb = NULL;
+
 	// Take Picture with Camera
 	fb = esp_camera_fb_get();  
 	if(!fb) 
@@ -120,16 +115,11 @@ void setup()
 		Serial.println("Camera capture failed");
 		return;
 	}
-	// initialize EEPROM with predefined size
-	EEPROM.begin(EEPROM_SIZE);
-	pictureNumber = EEPROM.read(0) + 1;
 
 	// Path where new picture will be saved in SD Card
-	String path = "/picture" + String(pictureNumber) +".jpg";
+	String path = "/picture" + String(p_pictureNumber) +".jpg";
 
 	fs::FS &fs = SD_MMC; 
-	Serial.printf("Picture file name: %s\n", path.c_str());
-	
 	File file = fs.open(path.c_str(), FILE_WRITE);
 
 	if(!file)
@@ -140,8 +130,6 @@ void setup()
 	{
 		file.write(fb->buf, fb->len); // payload (image), payload length
 		Serial.printf("Saved file to path: %s\n", path.c_str());
-		EEPROM.write(0, pictureNumber);
-		EEPROM.commit();
 	}
 	file.close();
 	esp_camera_fb_return(fb); 
@@ -150,12 +138,54 @@ void setup()
 	pinMode(4, OUTPUT);
 	digitalWrite(4, LOW);
 	rtc_gpio_hold_en(GPIO_NUM_4);
+}
+
+void setup() 
+{
+	WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); //disable brownout detector
 	
+	Serial.begin(115200);
+	Serial.print("hello");
+	//Serial.setDebugOutput(true);
+	//Serial.println();
+	
+	if(!checkSD())
+	{
+		return;
+	}
+
+	camera_config_t camConf;
+	configureCamera(camConf);
+
+	// Init Camera
+	esp_err_t err = esp_camera_init(&camConf);
+	if (err != ESP_OK) 
+	{
+		Serial.printf("Camera init failed with error 0x%x", err);
+		return;
+	}
+
+	// initialize EEPROM with predefined size
+	EEPROM.begin(EEPROM_SIZE);
+	pictureNumber = EEPROM.read(0) + 1;
+
+	// for(int brightnessLevel = -2; brightnessLevel <= 2; brightnessLevel += 1)
+	// {
+	int brightnessLevel = 0;
+	pictureNumber += 1;
+
+	// sensor_t *s = esp_camera_sensor_get();
+	// s->set_brightness(s, brightnessLevel);
+
+	takePicture(pictureNumber);
+	// }
+
+	EEPROM.write(0, pictureNumber);
+	EEPROM.commit();
+
 	delay(2000);
-	Serial.println("Going to sleep now");
-	delay(2000);
+	
 	esp_deep_sleep_start();
-	Serial.println("This will never be printed");
 }
 
 void loop() 
