d3be31e6e8421b273f0a5497a6059bdd902b3e45
Mircea Ionescu <mihail.mircea12@gmail.com>
2024-12-03 00:00:48 +0200

    tested the code here

--- detect-signs/c++/main_detect.cpp
+++ detect-signs/c++/main_detect.cpp
@@ -25,10 +25,46 @@ int main(int argc, char** argv)
 	} 
 
 	cv::Mat hsv_image;
-	cv::cvtColor(image, hsv_image, cv::COLOR_BGR2HSV); 
-
+	
 	std::chrono::time_point start = std::chrono::high_resolution_clock::now();
+    cv::Mat red_pixels = cv::Mat::zeros(image.size(), CV_8UC1);
 
+	uint32_t gw_detected = detect_gw_cv(image, red_pixels);
+	// uint32_t gw_detected = detect_gw(image);
+
+	std::cout << "found " << gw_detected << " give way signs" << std::endl;
+
+	std::chrono::time_point end = std::chrono::high_resolution_clock::now();
+	std::chrono::duration<double, std::milli> duration = end - start;
+
+	std::cout << "detect gw took " << duration.count() << " ms" << std::endl;
+
+	// ------------ show img 
+	cv::namedWindow("Display Image", cv::WINDOW_GUI_NORMAL); 
+	// cv::imshow("Display Image", resized_img); 
+	cv::imshow("Display Image", image); 
+	cv::waitKey(0); 
+
+	// ----------- shwo mask
+	// cv::namedWindow("Display Image", cv::WINDOW_GUI_NORMAL); 
+	// // cv::imshow("Display Image", resized_img); 
+	// cv::imshow("Display Image", red_pixels); 
+	// cv::waitKey(0); 
+
+	// // cv::Mat resized_img;
+	// // float scale_for_print = 0.6;
+	// // cv::resize(image, resized_img, cv::Size(), scale_for_print , scale_for_print);
+
+	// cv::namedWindow("Display Image", cv::WINDOW_GUI_NORMAL); 
+	// // cv::imshow("Display Image", resized_img); 
+	// cv::imshow("Display Image", image); 
+	// cv::waitKey(0); 
+
+	return 0; 
+}
+
+
+// code for checking each pixel in an image
 	// int found_red = 0;
 	// for (int i = 0; i < image.rows; i++)
 	// {
@@ -49,24 +85,4 @@ int main(int argc, char** argv)
 	// }
 	// std::cout << "found " << found_red << " red or white" << std::endl;
 	// std::cout << "total pixels: " << image.cols * image.rows << std::endl;
-
-	uint32_t gw_detected = detect_gw(image);
-
-	std::cout << "found " << gw_detected << " give ways" << std::endl;
-
-	std::chrono::time_point end = std::chrono::high_resolution_clock::now();
-	std::chrono::duration<double, std::milli> duration = end - start;
-
-	std::cout << "detect gw took " << duration.count() << " ms" << std::endl;
-	// cv::Mat resized_img;
-	// float scale_for_print = 0.6;
-	// cv::resize(image, resized_img, cv::Size(), scale_for_print , scale_for_print);
-
-	cv::namedWindow("Display Image", cv::WINDOW_GUI_NORMAL); 
-	// cv::imshow("Display Image", resized_img); 
-	cv::imshow("Display Image", image); 
-	cv::waitKey(0); 
-
-	return 0; 
-}
 
