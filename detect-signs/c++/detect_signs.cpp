1b0c60def33bdba16e0a325bdd2731c336e29a33
Mircea <mihail.mircea12@gmail.com>
2024-12-07 16:25:06 +0200

    changed the name of a parameter

--- detect-signs/c++/detect_signs.cpp
+++ detect-signs/c++/detect_signs.cpp
@@ -1,6 +1,5 @@
 #include "detect_signs.h"
 
-// iau din coltul din stanga sus labelul formei curente si verific cu asta la is red
 float check_for_gw_cv(cv::Mat &p_hsv_img, give_way_chunk p_chunk, cv::Mat &p_label_mat, int32_t p_label)
 {
     // filter out triangles with angle smaller than minimum
@@ -91,12 +90,17 @@ float check_for_gw_cv(cv::Mat &p_hsv_img, give_way_chunk p_chunk, cv::Mat &p_lab
     }
 
     // if there are a lot of red pixels outside of triangle, likely not a give way
-    if (red_outside_gw / (double)px_outside_gw > RED_OUTSIDE_GW_THRESHOLD)
+    if(px_outside_gw != 0)
     {
-        // too much red outside
-        return 0;
+        if ( red_outside_gw / (double)px_outside_gw > RED_OUTSIDE_GW_THRESHOLD)
+        {
+            // too much red outside
+            return 0;
+        }
+ 
     }
-   
+
+  
     // get final score for thin
     double thin_white_area = chunk_area(thin_inner_triangle);
     double thin_red_area = (chunk_area(p_chunk) - thin_white_area);
@@ -124,12 +128,12 @@ void print_detection(cv::Mat &p_img, give_way_chunk gw_chunk)
     cv::line(p_img, gw_chunk.top_right.get_cv_point(), gw_chunk.bottom.get_cv_point(), color, thickness);
 }
 
-float find_sign(cv::Mat &p_img, cv::Mat &p_hsv_img, cv::Mat &p_labels, cv::Mat &p_stats, int32_t p_pos)
+float find_sign(cv::Mat &p_img, cv::Mat &p_hsv_img, cv::Mat &p_labels, cv::Mat &p_stats, int32_t p_label)
 {
-    int32_t x = p_stats.at<int32_t>(cv::Point(0, p_pos));
-    int32_t y = p_stats.at<int32_t>(cv::Point(1, p_pos));
-    int32_t w = p_stats.at<int32_t>(cv::Point(2, p_pos));
-    int32_t h = p_stats.at<int32_t>(cv::Point(3, p_pos));
+    int32_t x = p_stats.at<int32_t>(cv::Point(0, p_label));
+    int32_t y = p_stats.at<int32_t>(cv::Point(1, p_label));
+    int32_t w = p_stats.at<int32_t>(cv::Point(2, p_label));
+    int32_t h = p_stats.at<int32_t>(cv::Point(3, p_label));
 
     if (h * w < MIN_BOUNDING_BOX_AREA)
     {
@@ -145,7 +149,7 @@ float find_sign(cv::Mat &p_img, cv::Mat &p_hsv_img, cv::Mat &p_labels, cv::Mat &
     point left_pt;
     for (int i = y; i < y + h; i++)
     {
-        if (p_labels.at<int32_t>(i, x) == p_pos)
+        if (p_labels.at<int32_t>(i, x) == p_label)
         {
             left_pt.x = x;
             left_pt.y = i;
@@ -156,7 +160,7 @@ float find_sign(cv::Mat &p_img, cv::Mat &p_hsv_img, cv::Mat &p_labels, cv::Mat &
     point right_pt;
     for (int i = y; i < y + h; i++)
     {
-        if (p_labels.at<int32_t>(i, x + w-1) == p_pos)
+        if (p_labels.at<int32_t>(i, x + w-1) == p_label)
         {
             right_pt.x = x + w-1;
             right_pt.y = i;
@@ -167,7 +171,7 @@ float find_sign(cv::Mat &p_img, cv::Mat &p_hsv_img, cv::Mat &p_labels, cv::Mat &
     point bottom_pt;
     for (int j = x; j < x + w; j++)
     {
-        if (p_labels.at<int32_t>(y + h-1, j) == p_pos)
+        if (p_labels.at<int32_t>(y + h-1, j) == p_label)
         {
             bottom_pt.x = j;
             bottom_pt.y = y + h-1;
@@ -178,7 +182,7 @@ float find_sign(cv::Mat &p_img, cv::Mat &p_hsv_img, cv::Mat &p_labels, cv::Mat &
     give_way_chunk gw_chunk = give_way_chunk(left_pt, right_pt, bottom_pt);
     // print_detection(p_img, gw_chunk);
 
-    float chunk_score = check_for_gw_cv(p_hsv_img, gw_chunk, p_labels, p_pos);
+    float chunk_score = check_for_gw_cv(p_hsv_img, gw_chunk, p_labels, p_label);
     if(chunk_score > MIN_CHUNK_SCORE)
     {
         print_detection(p_img, gw_chunk);
@@ -192,17 +196,17 @@ float find_sign(cv::Mat &p_img, cv::Mat &p_hsv_img, cv::Mat &p_labels, cv::Mat &
 void get_red_pixels(cv::Mat &p_hsv_img, cv::Mat &p_red_pixels)
 {
     // Define the lower and upper bounds for red hues in HSV
-    cv::Scalar lower_red_1(0, DARK_MIN_RED_SATURATION, DARK_MIN_RED_VALUE);  // Lower bound for red (0-10 degrees hue)
-    cv::Scalar upper_red_1(ALLOWED_RED_HUE_OFFSET, MAX_SATURATION, VALUE_DELIMITER); // Upper bound for red (0-10 degrees hue)
+    cv::Scalar lower_red_1(0, DARK_MIN_RED_SATURATION, DARK_MIN_RED_VALUE);  
+    cv::Scalar upper_red_1(ALLOWED_RED_HUE_OFFSET, MAX_SATURATION, VALUE_DELIMITER); 
 
-    cv::Scalar lower_red_2(0, BRIGHT_MIN_RED_SATURATION, VALUE_DELIMITER); // Lower bound for red (170-179 degrees hue)
-    cv::Scalar upper_red_2(ALLOWED_RED_HUE_OFFSET, MAX_SATURATION, MAX_VALUE); // Upper bound for red (170-179 degrees hue)
+    cv::Scalar lower_red_2(0, BRIGHT_MIN_RED_SATURATION, VALUE_DELIMITER); 
+    cv::Scalar upper_red_2(ALLOWED_RED_HUE_OFFSET, MAX_SATURATION, MAX_VALUE); 
 
-    cv::Scalar lower_red_3(MAX_HUE - ALLOWED_RED_HUE_OFFSET, DARK_MIN_RED_SATURATION, DARK_MIN_RED_VALUE);  // Lower bound for red (0-10 degrees hue)
-    cv::Scalar upper_red_3(MAX_HUE, MAX_SATURATION, VALUE_DELIMITER); // Upper bound for red (0-10 degrees hue)
+    cv::Scalar lower_red_3(MAX_HUE - ALLOWED_RED_HUE_OFFSET, DARK_MIN_RED_SATURATION, DARK_MIN_RED_VALUE);  
+    cv::Scalar upper_red_3(MAX_HUE, MAX_SATURATION, VALUE_DELIMITER); 
 
-    cv::Scalar lower_red_4(MAX_HUE - ALLOWED_RED_HUE_OFFSET, BRIGHT_MIN_RED_SATURATION, VALUE_DELIMITER); // Lower bound for red (170-179 degrees hue)
-    cv::Scalar upper_red_4(MAX_HUE, MAX_SATURATION, MAX_VALUE); // Upper bound for red (170-179 degrees hue)
+    cv::Scalar lower_red_4(MAX_HUE - ALLOWED_RED_HUE_OFFSET, BRIGHT_MIN_RED_SATURATION, VALUE_DELIMITER); 
+    cv::Scalar upper_red_4(MAX_HUE, MAX_SATURATION, MAX_VALUE); 
 
     // Create two masks for the two red hue ranges
     cv::Mat mask;
