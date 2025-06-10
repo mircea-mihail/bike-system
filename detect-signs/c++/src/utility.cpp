dd4b09b3701efc3901509ccbd68d538990d60ea9
Mircea Ionescu <mihail.mircea12@gmail.com>
2025-06-10 16:34:25 +0300

    fixed a display bug for crossing

--- detect-signs/c++/src/utility.cpp
+++ detect-signs/c++/src/utility.cpp
@@ -166,6 +166,26 @@ bool has_small_angle(give_way_chunk p_chunk)
     return min_triangle_condition;
 }
 
+bool has_small_angle(crossing_chunk p_chunk)
+{
+    float a_side = sqrt(pow(p_chunk.left.x - p_chunk.right.x, 2) + pow(p_chunk.left.y - p_chunk.right.y, 2));
+    float b_side = sqrt(pow(p_chunk.top.x - p_chunk.right.x, 2) + pow(p_chunk.top.y - p_chunk.right.y, 2));
+    float c_side = sqrt(pow(p_chunk.left.x - p_chunk.top.x, 2) + pow(p_chunk.left.y - p_chunk.top.y, 2));
+
+    if(a_side == 0 || b_side == 0 || c_side == 0)
+    {
+        return true;
+    }
+
+    float a_angle = acos((pow(b_side, 2) + pow(c_side, 2) - pow(a_side, 2)) / (2 * b_side * c_side)) * 180 / M_PI;
+    float b_angle = acos((pow(a_side, 2) + pow(c_side, 2) - pow(b_side, 2)) / (2 * a_side * c_side)) * 180 / M_PI;
+    float c_angle = acos((pow(b_side, 2) + pow(a_side, 2) - pow(c_side, 2)) / (2 * b_side * a_side)) * 180 / M_PI;
+
+    bool min_triangle_condition = (a_angle < MIN_TRIANGLE_ANGLE || b_angle < MIN_TRIANGLE_ANGLE || c_angle < MIN_TRIANGLE_ANGLE);
+
+    return min_triangle_condition;
+}
+
 void print_give_way(cv::Mat &p_img, give_way_chunk gw_chunk, float p_score)
 {
     cv::Scalar color = cv::Scalar(0, 255, 0);
@@ -236,6 +256,24 @@ void print_no_bikes(cv::Mat &p_img, no_bikes_chunk nb_chunk, float p_score)
     strcat(stringed_score, buf);
 
     cv::Point text_pt(nb_chunk.top.x, nb_chunk.top.y);
+    int font_size = 3;
+    cv::putText(p_img, stringed_score, text_pt, 1, font_size, cv::Scalar(0, 0, 0), 8);   
+    cv::putText(p_img, stringed_score, text_pt, 1, font_size, cv::Scalar(0, 255, 0), 3);   
+}
+
+void print_crossing(cv::Mat &p_img, crossing_chunk cr_chunk, float p_score)
+{
+    cv::Scalar color = cv::Scalar(0, 255, 0);
+    int thickness = 2;
+
+    cv::line(p_img, cr_chunk.top.get_cv_point(), cr_chunk.left.get_cv_point(), color, thickness);
+    cv::line(p_img, cr_chunk.left.get_cv_point(), cr_chunk.right.get_cv_point(), color, thickness);
+    cv::line(p_img, cr_chunk.right.get_cv_point(), cr_chunk.top.get_cv_point(), color, thickness);
+
+    char stringed_score[5];
+    snprintf(stringed_score, sizeof(stringed_score), "%.1f", p_score * 100);
+
+    cv::Point text_pt(int((cr_chunk.left.x + cr_chunk.right.x)/2), cr_chunk.top.y);
     int font_size = 3;
     cv::putText(p_img, stringed_score, text_pt, 1, font_size, cv::Scalar(0, 0, 0), 8);   
     cv::putText(p_img, stringed_score, text_pt, 1, font_size, cv::Scalar(0, 255, 0), 3);   
